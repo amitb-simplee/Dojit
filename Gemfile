f0e203eabc12f33b380f1c56623cc76b36794206
amitb <amit.b@simplee.com>
2017-02-15 16:57:33 +0200

    authorization

--- Gemfile
+++ Gemfile
@@ -33,9 +33,10 @@ group :assets do
 
   gem 'uglifier', '>= 1.0.3'
 end
+
 gem 'strong_parameters'
 gem 'jquery-rails'
-
+gem 'pundit'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   # attr_accessible :title, :body
+ attr_accessible :name, :email, :password, :password_confirmation 
+ def admin?
+   role == 'admin'
+ end
+ 
+ def moderator?
+   role == 'moderator'
+ end
 end
