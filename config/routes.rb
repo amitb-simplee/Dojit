12acd9fcb30580d075be9f9dad9d4b30d865b3a7
amitb <amit.b@simplee.com>
2017-02-16 08:49:41 +0200

    small refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Dojit::Application.routes.draw do
   get "topics/edit"
 
   devise_for :users
-
+  resources :users, only: [:update]
+  
   resources :topics do
     resources :posts, except: [:index]
   end
